feb6ca6e942529842c182609dd8de6e8f401574a
Paweł Darulewski <ray3nn@gmail.com>
2017-10-12 11:29:09 +0200

    More fileds in Recipe

--- Cookbook/Ingredient.cs
+++ Cookbook/Ingredient.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Cookbook
 {
-    class Ingridient
+    class Ingredient
     {
     }
 }
--- Cookbook/Recipe.cs
+++ Cookbook/Recipe.cs
@@ -8,8 +8,26 @@ namespace Cookbook
 {
     class Recipe
     {
-        private String title { set; get; }
-        private String recipeText { set; get; }
+        private string title { set; get; }
+        private string recipeText { set; get; }
+        private Dictionary<Ingredient, String> ingredientList { set; get; }
+        //Type of ingredient and its amount (e.g. "1", "2" but also "spoon", "glass" etc.)
+        private int portions { set; get; }
+        private TimeSpan cookingTime { set; get; }
+        private int _difficulty;
+        public int Difficulty
+        {
+            get
+            {
+                return _difficulty;
+            }
+            set
+            {
+                if (value < 0 || value > 10)
+                    throw new ArgumentOutOfRangeException("New value is out of range. Avaiable range is <0, 10>");
+                _difficulty = value;
+            }
+        }
 
     }
 }
